a02aa093660bdfacec4b7f78dabd10bd27ba735b
pou-belle <markellazax@gmail.com>
2019-05-23 16:06:13 +0200

    Update metaquast1.sh

--- scripts/metaquast1.sh
+++ scripts/metaquast1.sh
@@ -13,6 +13,6 @@ module load quast
 
 
 # Your commands
- metaquast.py  ~/Genome_Analysis/results/assembly/megahit_results1/final.contigs.fa -1 ~/Genome_Analysis/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/Genome_Analysis/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz        
+ metaquast.py  ~/Genome_Analysis/results/assembly/megahit_results1/final.contigs.fa -o  ~/Genome_Analysis/results/metaquast -t 2  -1 ~/Genome_Analysis/raw_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz -2 ~/Genome_Analysis/raw_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz        
 
 
